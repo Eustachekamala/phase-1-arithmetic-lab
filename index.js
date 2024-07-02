72b69828c4d3f1aaf6b0853ad6ea93b0e53fc918
eustachekamala <eustachejared25@gmail.com>
2024-07-02 13:38:28 +0300

    Assignment was done, of arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+console.log(multiply = num1 * num2);
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 20; 
+let num4 = 4
+
+let mod = (num3 % num4) + 4;
+
+console.log(`The remainder of ${num3} divided by ${num4} is ${mod}`);
+
+
+let max = Math.max(1,4,6,10,20);
+console.log(max);
